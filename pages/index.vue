3f9c7bc3f5222fa9939f9d6cc72501b1ed175cae
Enok Madrid <enokslaboratory@gmail.com>
2020-04-04 15:19:19 -0700

    add netlify functions

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,7 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        pupusas2go
-      </h1>
-      <h2 class="subtitle">
-        simple ecommerce site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">pupusas2go</h1>
     </div>
   </div>
 </template>
